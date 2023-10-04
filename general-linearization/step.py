c282e1d7aa17fc58bd407eaa03db024c2b10579a
M.M.R <140324657+MuhammadRiyanMadya@users.noreply.github.com>
2023-10-05 04:30:05 +0700

    step calculation path.py

--- general-linearization/step.py
+++ general-linearization/step.py
@@ -20,8 +20,22 @@ def file(
     df_np = df_exc.values[:int(bottom_row)]
 
     #check if the data is normal without any NAN value or the length is different
-    ...
     return df_exc, df_np
+
+def model(name, y,t,uf,Kp,taup,thetap):
+    if name == 'foptd'
+        try:
+            if (t-thetap) <= 0 :
+                um = uf(0)
+            else:
+                um = uf(t-thetap)
+        except:
+            um = u[0]
+        dydt = (Kp*(um-u[0]) - (y-vt[0]))/taup
+        #dydt = (Kp*um - y)/taup
+    #if name == ' '
+    return dydt
+
 
 def valve(df_np):
     t = df_np[:,0]
@@ -46,7 +60,7 @@ def valve(df_np):
     # 5. Use soptd model
 
     
-    max_OP = max(OP)
+    max_point = max(OP)
     min_OP = min(OP)
     max_PV = max(OP)
     min_PV = min(OP)
@@ -56,8 +70,28 @@ def valve(df_np):
     for i in range(len(valve_OP)):
         delta_OP = OP[i+1] - OP[i]
         delta_PV = PV[i+1] - PV[i]
-        
-    return valve_OP, valve_PV
+    return 
+def foptd_solver(x):
+    # x is a list containing three foptd parameter: Kp, taup, and thetap
+    Kp = x[0]
+    taup = x[1]
+    thetap = x[2]
+    yt = np.zeros(num_index)
+    yt[0] = 0 #vt[0]
+    for i in range(num_index-1):
+        tstep = [t[i],t[i+1]]
+        y = odeint(foptd,yt[i],tstep,args=(uf,Kp,taup,thetap)) #why if i put uf(i) the function is not working
+        yt[i+1] = y[-1]
+        yt[num_index-1] = yt[num_index-2]    
+    return yt
+
+def objective(x):
+    # x is a list containing three foptd parameter: Kp, taup, and thetap
+    yt = foptd_solver(x)
+    SSE = 0
+    for i in range(num_index):
+        SSE = SSE + (vt[i] - yt[i])**2
+    return SSE
 
 def graphic(Valve_OP, valve_PV)
     return graph

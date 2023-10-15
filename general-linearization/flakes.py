14020f30aca38d7529d6a415312bba9dc1c8a255
M.M.R <140324657+MuhammadRiyanMadya@users.noreply.github.com>
2023-10-15 21:31:47 +0700

    Update flakes.py

--- general-linearization/flakes.py
+++ general-linearization/flakes.py
@@ -14,14 +14,15 @@ class flakes():
         self.Kp     = None
         self.taup   = None
         self.thetap = None
-        self.u      = {}
+        self.t      = []
+        self.u      = []
         self.pv     = None
         
-    def model(self,y,t,uf,Kp,taup,thetap):
+    def __model(self,y,t,uf,Kp,taup,thetap):
 
         if self.name == 'fopdt':
             try:
-                if (time - thetap) < 0:
+                if (t - thetap) < 0:
                     u = uf(0)
                 else:
                     u = uf(t - thetap)
@@ -30,68 +31,52 @@ class flakes():
             dydt = (Kp*u - y)/taup
             return dydt
 
-    def response(step:dict,
-                Kp,
-                taup,
-                thetap = 0 ,
-                filename = 'Step_Data_1', save = False):
+    def response(self,
+                 step       :dict,
+                 Kp         :int,
+                 taup       :int,
+                 thetap     :int = 0,
+                 IV         :int = 0,
+                 Input0     :int = 0,
+                 time0      :int = 0,
+                 filename   :str = 'Step_Data_1',
+                 save       :bool = False
+                 ):
         
         self.Kp = Kp
         self.taup = taup
         self.thetap = thetap
-        t = []
-        for i in range(0, self.time + 1):
-                t.append(i)    
+
+        for i in range(time0, self.time + 1):
+                self.t.append(i)    
         
-        self.u = []
         for i in range(0, self.time + 1):
-                self.u.append(0)
-        for keys,values in step():
+                self.u.append(Input0)
+        for keys,values in step.items():
             for n in range(keys,len(self.u)):
-                self.u[keys]= values
+                self.u[n]= values
             
-        uf = interpolate.interp1d(t,u)
+        uf = interpolate.interp1d(self.t,self.u)
         
-        self.pv = [0]
+        self.pv = [IV]
         for i in range(0,self.time + 1):
             if i < 1:
                 delta_t = [0,1]
             else:
-                delta_t = [t[i-1],t[i]]
-            yt = integrate.odeint(self.model(,pv[i],delta_t,args=(uf,self.Kp,self.taup,self.thetap))
+                delta_t = [self.t[i-1],self.t[i]]
+            yt = integrate.odeint(self.__model,self.pv[i],delta_t,args=(uf,self.Kp,self.taup,self.thetap))
             self.pv.append(yt[1,0])
 
         del self.pv[0]
-        dt = [t, self.u, self.pv]
-        
-        # ------------------- #
-        
-        if save == True:
-            try: 
-                df = pd.DataFrame(dt).transpose()
-                df.columns = ['Time', 'OP','PV']
-                print(df)
-                df.to_excel(filename + '.xlsx')
-            except:
-                "Please input your filename"
+        dt = [self.t, self.u, self.pv]
+
         return dt
 
-    def graph(t,op,pv,show = True ):
-        if len(t) == len(op) == len(pv) and show == True:
-            plt.figure(1)
-            plt.subplot(2,1,1)
-            plt.plot(t,pv,'b-',linewidth = 3,label='process variable')
-            plt.legend()
-            plt.ylabel('% pv')
-            plt.subplot(2,1,2)
-            plt.plot(t,op,'g-',linewidth = 3,label='manipulated variable')
-            plt.legend()
-            plt.ylabel('% op')
-            plt.xlabel('Time')
-            plt.show()
+    def graph(self,data: dict, show = True ):
+        for i in range(1, len(data) + 1):
+            plt.figure(i)
+            for n in data:
+                plt.plot(n[0],n[1],label= 'process')
+                if show == True:
+                    plt.show()
         
-dt_1 = process(100,[0,30], save = True)
-
-dt_2 = process(100,[0,30], save = True)
-
-graph(dt[0],dt[1],dt[2], show = False)
